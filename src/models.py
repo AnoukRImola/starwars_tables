75882098f86a01e77120ca6e689742e6ae3edf67
AnoukRImola <anouk138@gmail.com>
2021-02-23 18:58:42 +0000

    tablas star wars

--- src/models.py
+++ src/models.py
@@ -8,7 +8,7 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+"""class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -27,7 +27,62 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {}"""
+
+
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favoritos.personaje_name'))
+    gender = Column(String(250))
+    hair_color = Column(String(250))
+    birth_year = Column(String(250))
+    height = Column(Integer)
+    mass = Column(Integer)
+
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250) ,ForeignKey('favoritos.planeta_name'))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    gravity = Column(String(250))
+    population = Column(Integer)
+    climate = Column(String(250))    
+
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), ForeignKey('favoritos.usuario_name'))
+    genero = Column(String(250))
+    correo = Column(String(250))
+    telefono = Column(String(250))
+    password = Column(String(250))
+
+
+class Favoritos(Base):
+    __tablename__ = 'favoritos'
+    id = Column(Integer, primary_key=True)
+    personaje_name = (Integer, ForeignKey('personaje.name'))
+    planeta_name = (Integer, ForeignKey('planeta.name'))
+    usuario_name = (Integer, ForeignKey('usuario.name'))
+      
+    
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
+""""""""""""""""
+fav
+id - personaje_id - planeta-id - usuario_id
+1  -     anakin       -   null     -  AnoukRimola
+2  -     r2-d2        -  null      -  123123123
+3       null          gatito           123123123
+
+
+"""""""""""""""""
